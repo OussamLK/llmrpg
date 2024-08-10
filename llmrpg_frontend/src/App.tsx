e0560f02e81011126b189da34d13afb08f7f553d
Oussam <larkem.m.oussam@gmail.com>
2024-08-10 15:07:30 +0100

    fix a bug

--- llmrpg_frontend/src/App.tsx
+++ llmrpg_frontend/src/App.tsx
@@ -4,37 +4,39 @@ import {match, P} from 'ts-pattern'
 
 type Action = {prompt: string, success: number, failure: number}
 const testActions = [
-  {prompt: "You are in a forest you find a mushroom, eat it?", success: 10, failure: 25},
-  {prompt: "You are in a supermarket, open the exit door?", success: 0, failure: 50},
+  {prompt: "You are in a forest you find a mushroom, eat it?", success: 10, failure: -25},
+  {prompt: "You are in a supermarket, open the exit door?", success: 0, failure: -50},
 
 ]
 const stats = createContext<{health:number}>({health:100})
 
 function App() {
   const [health, setHealth] = useState(100)
-  const takeDamage = useCallback(function takeDamage(damage:number, health:number):void{
-    if (damage > health) setHealth(0)
-    else setHealth(prev=>prev-damage)
+  const updateHealth = useCallback(function updateHealth(healthDifference:number, health:number):void{
+    const new_health = health + healthDifference
+    if (new_health <  0) setHealth(0)
+    else if (new_health > 100) setHealth(100)
+    else setHealth(new_health)
   }, [])
   return (
     <stats.Provider value= {{health}}>
       <h1>The first of us</h1>
       <h2>Health: {health}</h2>
       <div style={{border: "solid", borderWidth:"0.1rem"}}>
-        {health > 0 ? <Alive takeDamage={takeDamage} /> : <Dead />}
+        {health > 0 ? <Alive updateHealth={updateHealth} /> : <Dead />}
       </div>
    </stats.Provider>
   )
 }
 
-function Alive({takeDamage}:{takeDamage: (damage:number, health:number)=>void}){
+function Alive({updateHealth}:{updateHealth: (healthhealthDifference:number, health:number)=>void}){
   const [prompt, setPrompt] = useState<string>(`You are in the middle of nowhere`)
   const [mode, setMode] = useState<Action[] | 'talk'>(testActions)
   return match(mode).with(
    P.array({prompt:P.string, success: P.number, failure: P.number}),
       (actions:Action[]) =>
           <ActionMode
-              takeDamage={takeDamage}
+              updateHealth={updateHealth}
               prompt={prompt}
               actions={actions}
           /> 
@@ -54,13 +56,13 @@ function PromptMode({prompt}:{prompt:string}){
       </div>
 }
 
-function ActionMode({prompt, actions, takeDamage}
-  :{prompt:string, actions:Action[], takeDamage: (damage:number, health:number)=>void}){
+function ActionMode({prompt, actions, updateHealth}
+  :{prompt:string, actions:Action[], updateHealth: (healthhealthDifference:number, health:number)=>void}){
 
 
   const {health} = useContext(stats)
   const processAction = useCallback(function processAction(action:Action){
-    takeDamage(action.failure, health)
+    updateHealth(action.failure, health)
   }, [health])
     return (<>
       <p>{prompt}</p>
