bb0ac04b67b9122e3f0c4adea644f3e9e9c3d397
Oussam <larkem.m.oussam@gmail.com>
2024-09-29 16:54:44 +0100

    add docker-compose

--- backend/index.ts
+++ backend/index.ts
@@ -7,7 +7,7 @@ const pydantic_schemata = fs.readFileSync('../pydantic_models/models.pydantic', 
 console.debug("got from pydantic", pydantic_schemata)
 
 const OPEN_AI_MODELS= {3.5: 'gpt-3.5-turbo-1106',  4:'gpt-4-1106-preview'}
-const OPEN_AI_MODEL = OPEN_AI_MODELS[3.5] //change model here
+const OPEN_AI_MODEL = OPEN_AI_MODELS[4] //change model here
 type CHATGPTMessage = OpenAI.Chat.Completions.ChatCompletionMessageParam
 
 
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const backendAddress = process.env.BACKEND_ADDRESS || "http://localhost:3000"
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server:{
     proxy:{
       '/api': {
-        target: "http://localhost:3000",
+        target: backendAddress,
         changeOrigin: true,
         rewrite: path=>path.replace(/^\/api/, '')
       }
